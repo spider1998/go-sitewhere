76e3ed4322f1c68058292262aea932e9f48fe5ba
刘凡 <2387805574@qq.com>
2019-07-29 20:53:10 +0800

    格式化代码

--- mqtt/pub/simple.go
+++ mqtt/pub/simple.go
@@ -12,6 +12,7 @@ import (
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 )
 
+//
 var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
 	fmt.Printf("TOPIC: %s\n", msg.Topic())
 	fmt.Printf("MSG: %s\n", msg.Payload())
