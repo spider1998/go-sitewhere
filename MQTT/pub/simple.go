a1cd869ba77768af9c5d4cae7aad19d578375847
刘凡 <2387805574@qq.com>
2019-07-29 19:17:45 +0800

    添加MQTT对接sitewhere

--- MQTT/pub/simple.go
+++ MQTT/pub/simple.go
@@ -18,18 +18,6 @@ var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
 }
 var fail_nums int = 0
 
-type Testmsg struct {
-	DeviceToken string `json:"deviceToken"`
-	Originator  string `json:"originator"`
-	Type        string `json:"type"`
-	Request     struct {
-		Latitude    string `json:"latitude"`
-		Longitude   string `json:"longitude"`
-		Elevation   string `json:"elevation"`
-		UpdateState bool   `json:"updateState"`
-		EventDate   string `json:"eventDate"`
-	} `json:"request"`
-}
 
 func main() {
 	//生成连接的客户端数
@@ -46,42 +34,31 @@ func main() {
 }
 
 func createTask(taskId int, wg *sync.WaitGroup) {
-	fmt.Println("ddd")
+	fmt.Println("start...")
 	defer wg.Done()
-	opts := MQTT.NewClientOptions().AddBroker("tcp://192.168.35.230:1883").SetUsername("admin").SetPassword("password")
+	opts := MQTT.NewClientOptions().AddBroker("tcp://192.168.35.230:1883")
 	opts.SetCleanSession(true)
-	opts.SetClientID("sample1")
+	opts.SetClientID("your_clientId")
 	/*	opts.SetDefaultPublishHandler(f)*/
 	opts.SetConnectTimeout(time.Duration(5) * time.Second)
 
 	//创建连接
 	c := MQTT.NewClient(opts)
-
 	if token := c.Connect(); token.WaitTimeout(time.Duration(60)*time.Second) && token.Wait() && token.Error() != nil {
 		fail_nums++
 		fmt.Printf("taskId:%d,fail_nums:%d,error:%s \n", taskId, fail_nums, token.Error())
 		return
 	}
 
-	//json消息示例
-	/*var msg Testmsg
-	msg.DeviceToken = "bracelet-002"
-	msg.Originator = "admin"
-	msg.Type = "DeviceLocation"
-	msg.Request.Elevation = "0"
-	msg.Request.EventDate = time.Now().Format(time.RFC3339)
-	msg.Request.Latitude = "40"
-	msg.Request.Longitude = "-80"
-	msg.Request.UpdateState = true*/
+	//头部
 	var header proto.DeviceEvent_Header
 	header.DeviceToken = &proto.GOptionalString{Value:"rest-token-4"}	//设备token
 	header.Originator = &proto.GOptionalString{Value:"admin"}			//操作人
 	//header.Command = proto.DeviceEvent_SendLocation
 	header.Command = proto.DeviceEvent_SendLocation						//命令
 
+	//请求体
 	var body1 proto.DeviceEvent_DeviceLocation
-	//body1.MeasurementName = &proto.GOptionalString{Value:"Latitude"}
-	//body1.MeasurementValue= &proto.GOptionalDouble{Value:42}
 	body1.Latitude = &proto.GOptionalDouble{Value:float64(39.31)}
 	body1.Longitude = &proto.GOptionalDouble{Value:float64(-85.52)}
 	body1.UpdateState = &proto.GOptionalBoolean{Value:true}
@@ -93,6 +70,7 @@ func createTask(taskId int, wg *sync.WaitGroup) {
 		"Longitude":"-98",
 	}
 
+	//编码
 	send1,err :=proto2.Marshal(&header)
 	if err != nil{
 		panic(err)
@@ -107,11 +85,13 @@ func createTask(taskId int, wg *sync.WaitGroup) {
 	send = append(send,byte(len(send2)))
 	send = append(send,send2...)
 	fmt.Println(send)
+
+	//发送
 	token := c.Publish("SiteWhere/ymzn/input/protobuf", 1, true,send)
 	if !token.Wait() {
 		panic(token.Error())
 	}
-	//c.Disconnect(250)
+	c.Disconnect(250)
 	fmt.Println("task ok!!")
 }
 
---
+++ MQTT/sub/simple.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+	p `github.com/gogo/protobuf/proto`
+	"os"
+	"os/signal"
+	`sdkeji/go_mqtt/proto`
+	"syscall"
+)
+
+var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
+	fmt.Printf("MSG: %s\n", msg.Payload())
+	var ss proto.DeviceEvent
+	_ = p.Unmarshal(msg.Payload(),&ss)
+	fmt.Println(ss)
+}
+
+
+func main() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	opts := MQTT.NewClientOptions().AddBroker("tcp://192.168.35.230:1883")
+	opts.SetClientID("sample5")
+	opts.SetDefaultPublishHandler(f)
+	topic := "SiteWhere/ymzn/input/protobuf"
+	opts.OnConnect = func(c MQTT.Client) {
+		if token := c.Subscribe(topic, 0, f); token.Wait() && token.Error() != nil {
+			panic(token.Error())
+		}
+	}
+	client := MQTT.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	} else {
+		fmt.Printf("Connected to server\n")
+	}
+	<-c
+}
